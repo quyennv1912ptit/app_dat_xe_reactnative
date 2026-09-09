69965a4d84b9e51df555cb3037af1b3261c5ba9f
quyennv1912ptit <quyennv19122006@gmail.com>
2026-09-09 15:12:19 +0700

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen
+      name="index"
+      options={
+        {
+          title: "Trang Chủ",
+          headerShown: true
+        }
+      }
+    />
+  </Stack>
 }
